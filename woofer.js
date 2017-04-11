5181dd13d0f85d96e87bec6b7c30ec8d135837ca
Erik Gillespie <erik.gillespie@gmail.com>
2017-04-11 11:26:27 -0400

    Updated UI

--- woofer.js
+++ woofer.js
@@ -20,7 +20,8 @@ function addWoofRow (woofKey, woof) {
   var clone = document.importNode(template.content, true)
 
   clone.querySelector('.row').id = woofKey
-  clone.querySelector('p').textContent = woof.text
+  clone.querySelector('.created-at').textContent = moment(woof.created_at).calendar()
+  clone.querySelector('.text').textContent = woof.text
   clone.querySelector('.btn-edit').addEventListener('click', showWoofTextbox)
   clone.querySelector('.btn-delete').addEventListener('click', deleteWoof)
   clone.querySelector('input').addEventListener('keyup', editWoof)
@@ -30,19 +31,18 @@ function addWoofRow (woofKey, woof) {
 }
 
 // Adds a new woof to the database
-function addWoof (woof) {
-  firebase.database().ref('woofs').push(woof)
+function addWoof (text) {
+  firebase.database().ref('woofs').push({
+    created_at: new Date().getTime(),
+    text: text
+  })
 }
 
 // Get woof text from input and pass it to addWoof
 function createWoof () {
   var text = woofText.value || ''
   if (!text.trim().length) return
-
-  addWoof({
-    created_at: new Date().getTime(),
-    text: text
-  })
+  addWoof(text)
 }
 
 // Make the textbox to edit a woof appear
@@ -50,8 +50,10 @@ function showWoofTextbox () {
   var row = this.parentElement.parentElement
   var textbox = row.querySelector('input')
   var form = textbox.parentElement
+  var text = row.querySelector('.text')
 
-  textbox.value = row.querySelector('p').textContent
+  textbox.value = row.querySelector('.text').textContent
+  text.className = text.className.replace('show', 'hidden')
   form.className = form.className.replace('hidden', 'show')
   textbox.focus()
   textbox.select()
@@ -60,9 +62,10 @@ function showWoofTextbox () {
 // If Enter was pressed, update woof in database,
 // If Escape was pressed, hide the textbox
 function editWoof (event) {
-  var row = this.parentElement.parentElement
+  var row = this.parentElement.parentElement.parentElement
   var textbox = row.querySelector('input')
   var form = textbox.parentElement
+  var text = row.querySelector('.text')
 
   if (event.keyCode === 13) {
     // Enter key pressed
@@ -70,14 +73,17 @@ function editWoof (event) {
   } else if (event.keyCode === 27) {
     // Escape key pressed
     form.className = form.className.replace('show', 'hidden')
+    text.className = text.className.replace('hidden', 'show')
   }
 }
 
 // Update the woof text in a row on the page
 function updateWoofRow (row, woof) {
   var form = row.querySelector('input').parentElement
+  var text = row.querySelector('.text')
   form.className = form.className.replace('show', 'hidden')
-  row.querySelector('p').textContent = woof.text
+  row.querySelector('.text').textContent = woof.text
+  text.className = text.className.replace('hidden', 'show')
 }
 
 // Remove a woof row from the page
